e3254f9c6cc76c765044a8629c49a8f83cfbdd4f
Ruan Carvalho <142753360+RuanCarvalhoo@users.noreply.github.com>
2026-08-22 17:31:53 -0300

    test: amplia teste de adicionarNota

--- test/models/Aluno.test.js
+++ test/models/Aluno.test.js
@@ -1,17 +1,20 @@
-import { describe, it, expect } from "@jest/globals";
+import Aluno from '../../src/models/Aluno.js';
 
-import Aluno from "../../src/models/Aluno.js";
+describe('Aluno', () => {
+  let aluno;
 
-describe("Aluno", () => {
+  beforeEach(() => {
+    aluno = new Aluno('Maria');
+  });
 
-    describe("adicionarNota", () => {
-        it("deve adicionar uma nota a lista de notas do aluno", () => {
-            const aluno = new Aluno("Maria");
+  test('adicionarNota adiciona a primeira nota', () => {
+    aluno.adicionarNota(8);
+    expect(aluno.notas).toEqual([8]);
+  });
 
-            aluno.adicionarNota(8);
-
-            expect(aluno.notas).toEqual([8]);
-        });
-    });
-
+  test('adicionarNota adiciona uma segunda nota', () => {
+    aluno.adicionarNota(8);
+    aluno.adicionarNota(6);
+    expect(aluno.notas).toEqual([8, 6]);
+  });
 });
