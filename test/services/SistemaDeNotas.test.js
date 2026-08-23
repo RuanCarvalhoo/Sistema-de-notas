b2df24c9186477cc54767385b6298a257e12570e
ErnandesCosta <jegc@discente.ifpe.edu.br>
2026-08-23 13:07:14 -0300

    test: amplia teste de soma

--- test/services/SistemaDeNotas.test.js
+++ test/services/SistemaDeNotas.test.js
@@ -4,4 +4,8 @@ describe('SistemaDeNotas', () => {
   test('deve retornar 2 quando somar 1 + 1', () => {
     expect(SistemaDeNotas.soma(1, 1)).toBe(2);
   });
+
+  test('deve retornar 5 quando somar 2 + 3', () => {
+    expect(SistemaDeNotas.soma(2, 3)).toBe(5);
+  });
 });
