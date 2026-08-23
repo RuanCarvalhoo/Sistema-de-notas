99872a5e1f82194e20525241002c6ace98d9d8cf
ErnandesCosta <jegc@discente.ifpe.edu.br>
2026-08-23 14:00:54 -0300

    test: adiciona teste de getTurma

--- test/services/SistemaDeNotas.test.js
+++ test/services/SistemaDeNotas.test.js
@@ -14,4 +14,21 @@ describe('SistemaDeNotas', () => {
 
     expect(sistema.getNomesTurmas()).toEqual(['1A', '1B', '2A']);
   });
+
+
+  test('getTurma retorna a turma quando ela existe', () => {
+    const sistema = new SistemaDeNotas(['1A']);
+
+    const turma = sistema.getTurma('1A');
+
+    expect(turma).not.toBeNull();
+    expect(turma.nome).toBe('1A');
+  });
+
+
+  test('getTurma retorna null quando a turma não existe', () => {
+    const sistema = new SistemaDeNotas(['1A']);
+
+    expect(sistema.getTurma('2B')).toBeNull();
+  });
 });
