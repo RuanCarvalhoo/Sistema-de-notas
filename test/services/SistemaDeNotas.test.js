23507d70ef2d3f8a2045dc7732d1b518e86c8a88
ErnandesCosta <jegc@discente.ifpe.edu.br>
2026-08-23 13:44:58 -0300

    test: adiciona teste de getNomesTurmas

--- test/services/SistemaDeNotas.test.js
+++ test/services/SistemaDeNotas.test.js
@@ -1,11 +1,10 @@
 import SistemaDeNotas from '../../src/services/SistemaDeNotas.js';
 
-describe('SistemaDeNotas', () => {
-  test('deve retornar 2 quando somar 1 + 1', () => {
-    expect(SistemaDeNotas.soma(1, 1)).toBe(2);
-  });
 
-  test('deve retornar 5 quando somar 2 + 3', () => {
-    expect(SistemaDeNotas.soma(2, 3)).toBe(5);
+describe('SistemaDeNotas', () => {
+  test('getNomesTurmas retorna os nomes das turmas cadastradas', () => {
+    const sistema = new SistemaDeNotas(['1A']);
+
+    expect(sistema.getNomesTurmas()).toEqual(['1A']);
   });
 });
